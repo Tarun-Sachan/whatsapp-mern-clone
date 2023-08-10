d31c33e6aa561090b3f563311c183c79007c82c7
tarunSachan <117214735+Tarun-Sachan@users.noreply.github.com>
2023-08-10 19:14:05 +0530

    Update axios.js

--- client/src/axios.js
+++ client/src/axios.js
@@ -3,7 +3,7 @@ import axios from "axios";
 const instance = axios.create({
   //baseURL: "http://localhost:9000",
   
-  baseURL: ${window.location.origin},
+  baseURL: "whatsapp-mern-clone-theta.vercel.app",
 });
 
 export default instance;
